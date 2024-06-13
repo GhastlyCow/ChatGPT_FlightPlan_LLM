e79e16a63d70938aec3fbc8b7a6a9dbfe1e3ca05
Pranav Gupta <p.gupta@duke.edu>
2024-06-13 13:39:44 -0400

    implemented few shot learning

--- main.py
+++ main.py
@@ -7,37 +7,10 @@ from langchain_core.prompts import PromptTemplate
 from langchain_core.runnables import RunnablePassthrough
 from langchain_core.output_parsers import StrOutputParser
 from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_core.prompts.few_shot import FewShotPromptTemplate
+from langchain_core.prompts.prompt import PromptTemplate
 import sys
 
-flight_plans = {
-
-    # You are an expert on determining the best flight plan. Given an input question, pick the best flight plan that satisifies the users constraints in a friendly way.
-    # The elements in the list are:
-    # D - Distance
-    # A - Altitude
-    # E - Energy Consumption
-    # W - Number of Waypoints
-    'flight_plan_1': [12.4, 1000, 1108.57, 10],
-    'flight_plan_2': [12.4, 1000, 1230.18, 11],
-    'flight_plan_3': [12.4, 1000, 1233.11, 13],
-    'flight_plan_4': [12.4, 1000, 1221.78, 15],
-
-    'flight_plan_5': [12.4, 1500, 1148.77, 9],
-    'flight_plan_6': [12.4, 1500, 1095.56, 10],
-    'flight_plan_7': [12.4, 1500, 1084.43, 11],
-    'flight_plan_8': [12.4, 1500, 1091.84, 14],
-
-    'flight_plan_9': [12.4, 2500, 1236.27, 10],
-    'flight_plan_10': [12.4, 2500, 1227.38, 11],
-    'flight_plan_11': [12.4, 2500, 1223.45, 13],
-    'flight_plan_12': [12.4, 2500, 1218.78, 15],
-
-    'flight_plan_13': [12.4, 3000, 1112.14, 9],
-    'flight_plan_14': [12.4, 3000, 1108.57, 10],
-    'flight_plan_15': [12.4, 3000, 1108.04, 12],
-    'flight_plan_16': [12.4, 3000, 1101.97, 14]
-
-}
 
 llm = ChatOpenAI(model="gpt-3.5-turbo-0125")
 
@@ -55,20 +28,81 @@ retriever = vectorstore.as_retriever(search_kwargs={"k": 2}) # search_type="simi
 def format_docs(docs):
     return "\n\n".join(doc.page_content for doc in docs)
 
-template = """
 
-{context}
+examples = [
 
-Question: {question}
+        {
+        "question": "I want to travel the shortest distance at the lowest altitude",
+        "context": "{context}",
+        "answer": """
+        Are follow up questions needed here: Yes.
+        Follow up: For every altitude, which is the shortest flight plan?
+        Intermediate Answer: 
+        'flight_plan_1': [12.1, 1000, 1108.57, 10],
+        'flight_plan_5': [15.1, 1500, 1148.77, 9],
+        'flight_plan_9': [17.1, 2500, 1236.27, 10],
+        'flight_plan_13': [19.1, 3000, 1112.14, 9],
+        Follow up: Which one is at the lowest altitude?
+        Intermediate Answer:
+        'flight_plan_1': [12.1, 1000, 1108.57, 10]
+        So the final answer is flight_plan_1.
+        """,
+    },
+     {
+        "question": "I want to use the least amount of energy while traveling the longest distance",
+        "context": "{context}",
+        "answer": """
+        Are follow up questions needed here: Yes.
+        Follow up: What are the 5 flight plans with the lowest energy consumption
+        Intermediate Answer: 
+        'flight_plan_7': [15.7, 1500, 1084.43, 11],
+        'flight_plan_8': [15.9, 1500, 1091.84, 14],
+        'flight_plan_6': [15.4, 1500, 1095.56, 10],
+        'flight_plan_16': [19.9, 3000, 1101.97, 14]
+        'flight_plan_15': [19.7, 3000, 1108.04, 12],
+        Follow up: Which one has the longest distance?
+        Intermediate Answer:
+        'flight_plan_16': [19.9, 3000, 1101.97, 14]
+        So the final answer is flight_plan_16.
+        """,
+    },
+    {
+        "question": "I want to take the least complex path that covers the longest distance",
+        "context": "{context}",
+        "answer": """
+        Are follow up questions needed here: Yes.
+        Follow up: What does complex mean?
+        Intermediate Answer: complex means waypoints.
+        Follow up: What are the flight paths with the lowest number of waypoints
+        Intermediate Answer: 
+        'flight_plan_5': [15.1, 1500, 1148.77, 9],
+        'flight_plan_13': [19.1, 3000, 1112.14, 9],
+        Follow up: Which one has the longest distance?
+        Intermediate Answer:
+        'flight_plan_13': [19.1, 3000, 1112.14, 9],
+        So the final answer is flight_plan_13.
+        """,
+    },
 
-Helpful Answer:"""
-# custom_rag_prompt = PromptTemplate.from_tempate(template)
+]
+
+example_prompt = PromptTemplate(
+    input_variables=["question", "context", "answer"], template="Question: {question}\n{answer}"
+)
+
+fsprompt = FewShotPromptTemplate(
+    examples=examples,
+    example_prompt=example_prompt,
+    suffix="Question: {question} {context}",
+    input_variables=["question", "context"],
+)
+
 prompt = hub.pull("rlm/rag-prompt")
 
 
 rag_chain = (
     {"context": retriever | format_docs, "question": RunnablePassthrough()}
-    | prompt
+    | fsprompt
     | llm
     | StrOutputParser()
     )
